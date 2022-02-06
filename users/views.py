9adee84849fddc7ad1fa87ff4b4883475b1ec990
Нуфтанчики <300225@mail.ru>
2022-02-06 22:04:27 +0300

    сделал авторизацию api/user/

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from  rest_framework.exceptions import AuthenticationFailed
 from .serialisers import UserSerialiser
 from .models import User
 import datetime, jwt
+from api.settings import SECRET_KEY
 
 class RegisterView(APIView):
     def post(self, request):
@@ -36,7 +37,7 @@ class LoginView(APIView):
             'iat': datetime.datetime.utcnow()
         }
 
-        token = jwt.encode(payload, "secret", algorithm="HS256")   #   decode('utf-8') в видео было, а тут не получается
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')   #   decode('utf-8') в видео было, а тут не получается
 
         response = Response()
         response.set_cookie(key='jwt', value=token, httponly=True)
@@ -44,4 +45,17 @@ class LoginView(APIView):
         return response
 
 
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Пользователь не авторизован!')
 
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Пользователь с таким токеном не авторизован!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerialiser(user)
+        return Response(serializer.data)
